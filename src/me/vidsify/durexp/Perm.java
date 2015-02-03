8f7fa96d1c3677f37fcba796c5b731ae7e2d2196
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-02-03 15:45:16 -0600

    Refactored to multiple classes.

---
+++ src/me/vidsify/durexp/Perm.java
@@ -0,0 +1,13 @@
+package me.vidsify.durexp;
+
+/**
+ * 
+ * @author Nikolai
+ */
+public abstract class Perm {
+    
+    public static final String ALLOW = "doubleurexp.allow";
+    public static final String ADMIN = "doubleurexp.use";
+    public static final String MULTIPLIER = "doubleurexp.multiplier.";
+    
+}
