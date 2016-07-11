14cdebe9fa6b0ed9f124aff9e3fb5c6eb8aca9cb
James H <me@exloki.com>
2016-07-11 14:14:12 +0100

    General code cleanup

--- src/me/vidsify/durexp/listeners/PlayerExpListener.java
+++ src/me/vidsify/durexp/listeners/PlayerExpListener.java
@@ -1,7 +1,5 @@
 package me.vidsify.durexp.listeners;
 
-import java.util.Calendar;
-import java.util.List;
 import me.vidsify.durexp.DurexpPlugin;
 import me.vidsify.durexp.Perm;
 import org.bukkit.Material;
@@ -10,90 +8,67 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
 /**
- *
+ * Main plugin listener class
  * @author Vidsify
  */
 public class PlayerExpListener implements Listener {
-
-    DurexpPlugin plugin;
+    private DurexpPlugin plugin;
 
     public PlayerExpListener(DurexpPlugin reference) {
         this.plugin = reference;
     }
 
     @EventHandler
-    public void onExpChange(PlayerExpChangeEvent e) {
-        Calendar mydate = Calendar.getInstance();
-        int dow = mydate.get(7);
-        Boolean alreadyDone = Boolean.valueOf(false);
-        List<String> EnabledDays = plugin.getConfig().getStringList("DaysToEnable");
+    public void onExpChange(PlayerExpChangeEvent event) {
+        if(!plugin.getConfig().getBoolean("Enable")) return; // Don't waste computation power if we're not even enabled
+
+        // Gather config variables
+        List<String> enabledDays = plugin.getConfig().getStringList("DaysToEnable");
         int radius = plugin.getConfig().getInt("CheckRadius");
-        for (String Day : EnabledDays) {
-            int currentDay = 0;
-            if (Day.toLowerCase().contains("mon")) {
-                currentDay = 2;
-            } else if (Day.toLowerCase().contains("tue")) {
-                currentDay = 3;
-            } else if (Day.toLowerCase().contains("wed")) {
-                currentDay = 4;
-            } else if (Day.toLowerCase().contains("thu")) {
-                currentDay = 5;
-            } else if (Day.toLowerCase().contains("fri")) {
-                currentDay = 6;
-            } else if (Day.toLowerCase().contains("sat")) {
-                currentDay = 7;
-            } else if (Day.toLowerCase().contains("sun")) {
-                currentDay = 1;
+
+        // Get today's day string (US locale) and check if it's enabled in the config, return if it's not
+        String currentDay = Calendar.getInstance().getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US);
+        boolean enabled = false;
+        for(String day : enabledDays) {
+            if(day.toLowerCase().contains(currentDay.toLowerCase())) {
+                enabled = true;
+                break;
             }
-            if ((dow == currentDay) && (!alreadyDone.booleanValue())
-                    && (plugin.getConfig().getBoolean("Enable"))) {
-                if (plugin.getConfig().getBoolean("CheckForSpawner")) {
-                    if (!plugin.isLocationNearBlock(e.getPlayer().getLocation(), Material.MOB_SPAWNER, radius)) {
-                        alreadyDone = Boolean.valueOf(true);
-                        Player player = e.getPlayer();
-                        int originalAmount = e.getAmount();
-                        int newAmount = originalAmount;
-                        if (plugin.getConfig().getBoolean("EnablePermMultiplier")) {
-                            boolean found = false;
-                            for (float temp = 0.0F; temp <= 10.0F; temp = (float) (temp + 0.1D)) {
-                                if (!player.isOp() && player.hasPermission(Perm.MULTIPLIER + temp)) {
-                                    newAmount = (int) (originalAmount * temp);
-                                    found = true;
-                                }
-                            }
-                            if ((!found)
-                                    && (player.hasPermission(Perm.ALLOW))) {
-                                newAmount = (int) (originalAmount * plugin.getConfig().getDouble("Multiplier"));
-                            }
-                        } else if (player.hasPermission(Perm.ALLOW)) {
-                            newAmount = (int) (originalAmount * plugin.getConfig().getDouble("Multiplier"));
-                        }
-                        e.setAmount(newAmount);
-                    }
-                } else {
-                    alreadyDone = Boolean.valueOf(true);
-                    Player player = e.getPlayer();
-                    int originalAmount = e.getAmount();
-                    int newAmount = originalAmount;
-                    if (plugin.getConfig().getBoolean("EnablePermMultiplier")) {
-                        boolean found = false;
-                        for (float temp = 0.0F; temp <= 10.0F; temp = (float) (temp + 0.1D)) {
-                            if (!player.isOp() && player.hasPermission(Perm.MULTIPLIER + temp)) {
-                                newAmount = (int) (originalAmount * temp);
-                                found = true;
-                            }
-                        }
-                        if ((!found)
-                                && (player.hasPermission(Perm.ALLOW))) {
-                            newAmount = (int) (originalAmount * plugin.getConfig().getDouble("Multiplier"));
-                        }
-                    } else if (player.hasPermission(Perm.ALLOW)) {
-                        newAmount = (int) (originalAmount * plugin.getConfig().getDouble("Multiplier"));
-                    }
-                    e.setAmount(newAmount);
+        }
+        if(!enabled) return; // Not enabled for today
+
+        // Check for spawners nearby and return if we find one
+        if(plugin.getConfig().getBoolean("CheckForSpawner")
+                && plugin.isLocationNearBlock(event.getPlayer().getLocation(), Material.MOB_SPAWNER, radius)) return;
+
+        // Multiply the experience gain
+        Player player = event.getPlayer();
+        int originalAmount = event.getAmount();
+        int newAmount = originalAmount;
+        boolean found = false;
+        if (plugin.getConfig().getBoolean("EnablePermMultiplier")) {
+            /* TODO:
+             * Consider making this go backwards so a permission with a higher multiplier
+             * will be caught before a permission with a lower multiplier. This is useful
+             * for servers which use inherited permissions. - Exloki
+             */
+            for (float temp = 0.0F; temp <= 10.0F; temp += 0.1F) {
+                if (!player.isOp() && player.hasPermission(Perm.MULTIPLIER + temp)) {
+                    newAmount = (int) (originalAmount * temp);
+                    found = true;
+                    break;
                 }
             }
-        }	
+        }
+        if (!found && player.hasPermission(Perm.ALLOW)) {
+            newAmount = (int) (originalAmount * plugin.getConfig().getDouble("Multiplier"));
+        }
+
+        event.setAmount(newAmount);
      }
 }
